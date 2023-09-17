5226b33e2f3afd7c3356bb7b647ae8fe5ea8c8f1
John Hollowell <jhollowe@johnhollowell.com>
2023-09-17 03:27:07 +0000

    Add sample_config.yaml

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,3 +5,6 @@ pre-commit install
 
 # run pre-commit on a simple file to ensure it downloads all needed tools
 pre-commit run --files .pre-commit-config.yaml
+
+# let ESPHome download needed dependencies and pre-compile all sources
+esphome compile sample_config.yaml
